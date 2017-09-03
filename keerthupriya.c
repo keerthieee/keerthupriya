b51f80dc9bb1e8c4b848bfdfee6d4b6766894a9e
keerthieee <31505484+keerthieee@users.noreply.github.com>
2017-09-03 10:58:09 +0530

    Create keerthupriya.c

---
+++ keerthupriya.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+{
+char c;
+scanf("%c",&c);
+if(c>='a'&&c<='z'||c>='A'&&C<='Z')
+{
+printf("it is an alphabet");
+}
+else
+{
+printf("it is not an alphabet");
+}
+}
